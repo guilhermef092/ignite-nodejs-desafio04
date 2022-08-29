3c4826bdb71ca7f8ca4d0eacd359ca87404c4efb
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2022-08-29 19:09:18 -0300

    Desafio 04 - Ignite NodeJS

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    if (!user.admin) {
+      throw new Error("User is not an administrator");
+    }
+
+    return this.usersRepository.list();
+  }
+}
+
+export { ListAllUsersUseCase };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ user_id }: IRequest): User {
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExists) {
+      throw new Error("User does not exist!");
+    }
+
+    const user = this.usersRepository.turnAdmin(userAlreadyExists);
+
+    return user;
+  }
+}
+
+export { TurnUserAdminUseCase };
